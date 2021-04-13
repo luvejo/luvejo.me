e27d89fec5cc131738ee35d6bb32048a1f8ea95f
Luis Velásquez <luvejo.dev@gmail.com>
2021-04-12 21:22:44 -0500

    build: add initial Webpack setup and project structure

---
+++ README.md
@@ -0,0 +1,20 @@
+# Luvejo.me
+
+My web dev web.
+
+## Installation
+
+```bash
+# Download the repo.
+git clone https://github.com/luvejo/luvejo.me.git
+```
+
+## Build Setup
+
+```bash
+# Install dependencies.
+$ npm install
+
+# Serve with hot reload at localhost:8080.
+$ npm run dev
+```
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'production',
+
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(s[ac]|c)ss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+    ],
+  },
+
+  devServer: {
+    contentBase: './dist',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+}
