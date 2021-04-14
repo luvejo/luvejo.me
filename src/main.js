2fe8d28cc0768ca19d24c79141d7cf8286f0c025
Luis Velásquez <luvejo.dev@gmail.com>
2021-04-14 16:47:58 -0500

    feat: move embedded script to main.js for Babel transpiling

--- src/main.js
+++ src/main.js
@@ -3,3 +3,42 @@ import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/brands'
 
 import './css/main.scss'
+
+// Mobile Menu Setup.
+function initMenu() {
+  const burger = document.querySelector('#burger')
+  const menuContainer = document.querySelector('.menu-mobile')
+  const menu = document.querySelector('.menu-mobile .menu')
+  const menuReplace = document.querySelector('.menu-bar-replace')
+  const menuLinks = document.querySelectorAll('.menu-mobile a')
+
+  function toggleMenu() {
+    menu.classList.toggle('active')
+    burger.classList.toggle('active')
+    menuContainer.classList.toggle('active')
+    menuReplace.classList.toggle('active')
+
+    const burgerIcons = document.querySelectorAll('#burger svg')
+
+    // Switch between icons:
+    burgerIcons.forEach((icon) => {
+      icon.classList.toggle('active')
+    })
+  }
+
+  burger.addEventListener('click', toggleMenu)
+  menuLinks.forEach((link) => {
+    link.addEventListener('click', toggleMenu)
+  })
+}
+
+// Detect if document is ready.
+function ready(callbackFunction) {
+  if (document.readyState != 'loading') {
+    callbackFunction(event)
+  } else {
+    document.addEventListener('DOMContentLoaded', callbackFunction)
+  }
+}
+
+ready(initMenu)
