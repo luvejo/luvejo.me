c6645a3e05a0e01aa22c38db1dee473651402968
Luis Velásquez <luvejo.dev@gmail.com>
2021-04-14 20:42:25 -0500

    feat: add image compression plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HTMLInlineCSSWebpackPlugin = require('html-inline-css-webpack-plugin')
   .default
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
+
 const path = require('path')
 
 module.exports = {
@@ -32,7 +34,7 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: 'asset',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
@@ -60,5 +62,12 @@ module.exports = {
       template: 'src/index.html',
     }),
     new HTMLInlineCSSWebpackPlugin(),
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        // Lossless optimization with custom option
+        // Feel free to experiment with options for better result for you
+        plugins: [['imagemin-mozjpeg', { quality: 90 }]],
+      },
+    }),
   ],
 }
